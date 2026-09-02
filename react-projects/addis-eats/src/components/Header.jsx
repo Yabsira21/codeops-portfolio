790a7c408f0bedfdd86b67e4c1918f52498585bc
Yabsira <Yabsira21116@gmail.com>
2026-09-03 00:13:54 +0300

    Day 29: add loading and error state mainly

--- react-projects/addis-eats/src/components/Header.jsx
+++ react-projects/addis-eats/src/components/Header.jsx
@@ -1,9 +1,20 @@
+import { useRef, useEffect } from "react";
+
 function Header() {
   const restaurantName = "Addis Café";
+  const searchRef = useRef(null);
+
+  useEffect(() => {
+    searchRef.current.focus();
+  }, []);
+
   return (
     <header>
       <h1>☕{restaurantName}</h1>
-      <h2>Fresh Ethiopian Food & Coffee</h2>
+      <h2>Fresh Ethiopian Food & Coffees</h2>
+      <form>
+        <input ref={searchRef} placeholder="search" />
+      </form>
     </header>
   );
 }
