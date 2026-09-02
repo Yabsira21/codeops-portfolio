e5bc9e7ced2d9886e015022965b7d9e291c8f4dd
Yabsira <Yabsira21116@gmail.com>
2026-09-02 14:48:59 +0300

    Day 26: Activities

--- react-projects/addis-eats/src/components/Dish.jsx
+++ react-projects/addis-eats/src/components/Dish.jsx
@@ -1,11 +1,20 @@
-function Dish({ name, price }) {
+import PropTypes from "prop-types";
+
+function Dish({ name, price, spicy }) {
   console.log(name, price);
   return (
     <div className="dish">
       <h3>{name}</h3>
       <p>{price} ETB</p>
+      <p>{spicy ? "Spicy" : "Not Spicy"}</p>
     </div>
   );
 }
+
+Dish.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  spicy: PropTypes.bool, // optional
+};
 
 export default Dish;
